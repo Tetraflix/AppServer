a6c99c0267e8ce5aa0e433cf76bdf10a9af6a745
arsimr16 <arsimr16@g.holycross.edu>
2017-10-31 14:59:25 -0700

    test mongodb genre recs

--- test/mongoDbTest.js
+++ test/mongoDbTest.js
@@ -6,26 +6,42 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+const genreArr = [
+  'action',
+  'animation',
+  'comedy',
+  'documentary',
+  'drama',
+  'family',
+  'fantasy',
+  'international',
+  'horror',
+  'musical',
+  'mystery',
+  'romance',
+  'sci_fi',
+  'thriller',
+  'western',
+];
 const id = Math.floor(Math.random() * 300000);
+const genre = Math.floor(Math.random() * 15);
 
 describe('UserMovies', () => {
-
   it('should contain at least 1,000,000 users', () => {
-    mongoDb.UserMovies.count({}, (err, result) => {
-    })
+    mongoDb.UserMovies.count()
       .then((count) => {
         const isBigEnough = count >= 1000000;
         isBigEnough.should.equal(true);
       })
       .catch((err) => {
-        console.error(err);
+        throw err;
       });
   });
 
   it('each user should have a recs list', () => {
     mongoDb.UserMovies.findById(id, (err, result) => {
       if (err) {
-        console.log(err);
+        throw err;
       }
       result.recs.should.be.a('object');
     });
@@ -34,10 +50,51 @@ describe('UserMovies', () => {
   it('each user should have a cw list', () => {
     mongoDb.UserMovies.findById(id, (err, result) => {
       if (err) {
-        console.log(err);
+        throw err;
       }
       result.cw.should.be.a('object');
     });
   });
+});
 
+describe('GenreRec', () => {
+  it('should only contain 15 documents', () => {
+    mongoDb.GenreRec.count()
+      .then((count) => {
+        count.should.equal(15);
+      });
+  });
+
+  it('each document should have a genre property', () => {
+    mongoDb.UserMovies.findOne({
+      where: {
+        genre: genreArr[genre],
+      },
+    })
+      .then((result) => {
+        result.should.be.a('string');
+      });
+  });
+
+  it('each document should have a recs property', () => {
+    mongoDb.UserMovies.findOne({
+      where: {
+        genre: genreArr[genre],
+      },
+    })
+      .then((result) => {
+        result.recs.should.be.a('array');
+      });
+  });
+
+  it('each document should have a createdAt property', () => {
+    mongoDb.UserMovies.findOne({
+      where: {
+        genre: genreArr[genre],
+      },
+    })
+      .then((result) => {
+        result.createdAt.should.be.a('string');
+      });
+  });
 });
