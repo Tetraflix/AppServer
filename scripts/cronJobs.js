7d16abdf5b12c8d21c37357aa39db37467d313d9
arsimr16 <arsimr16@g.holycross.edu>
2017-11-01 10:19:05 -0700

    refactor cron job

--- scripts/cronJobs.js
+++ scripts/cronJobs.js
@@ -2,12 +2,31 @@ const cron = require('node-cron');
 const userRecs = require('./simulateUserRecs.js');
 const genreRecs = require('./calculateGenreRecs');
 
-// this should run every minute
+// simultate GET requests for individual user recommendations
 cron.schedule('* * * * *', () => {
-  // I want this to run at a random interval between 50 and 200 ms
-  // will this calculate a new random value every minute?
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
+  setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
+});
+cron.schedule('* * * * *', () => {
   setInterval(userRecs, Math.floor(Math.random() * 150) + 50);
 });
 
-// this should run (on the 59th minute of) every hour
+// update genre recommendations every hour
 cron.schedule('* 59 * * * *', genreRecs);
